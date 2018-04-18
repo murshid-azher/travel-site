58500fd36ab05533844f0080eac7d718e0619a4e
murshid-azher <murshidazher@yahoo.com>
2018-04-18 17:46:11 +0530

    Just a commit

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssimport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 
 gulp.task('styles', function() {
 		return gulp.src('./app/assets/styles/styles.css')
-      .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+      .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 			.on('error', function(errorInfo) {
 				console.log(errorInfo.toString());
 				this.emit('end'); // gracefully saying that the task has come to an end.
