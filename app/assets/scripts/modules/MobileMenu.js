91718d2e2d54bc8839c7aab052add679cf6ab64f
murshid-azher <murshidazher@yahoo.com>
2018-04-20 12:17:07 +0530

    Made Hamburger Menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,7 +2,24 @@ import $ from 'jquery';
 
 class MobileMenu {
   constructor() {
-    
+    this.siteHeader = $('.site-header');
+    this.menuIcon = $('.site-header__menu-icon');
+    this.menuContent = $('.site-header__menu-content');
+    this.events(); //because events isn't a keyword in JS,
+                  //so we need to manually load so that it works once the page is loaded
+                  //it will call this function
+
+  }
+
+  events() {
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
+  }
+
+  toggleTheMenu() {
+    this.menuContent.toggleClass('site-header__menu-content--is-visible');
+    this.siteHeader.toggleClass('site-header--is-expanded');
+    this.menuIcon.toggleClass('site-header__menu-icon--close-x');
+
   }
 }
 
